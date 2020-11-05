6daef2e0ab5d6fa354cfb5e50f6e72821b9c315f
Fidel Salazar <fidelsf91@gmail.com>
2020-11-04 21:48:11 -0600

    first proposed solution

--- MinimumSwaps/Program.cs
+++ MinimumSwaps/Program.cs
@@ -7,24 +7,43 @@ namespace MinimumSwaps
     {
         static void Main(string[] args)
         {
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            int n = Convert.ToInt32(Console.ReadLine());
+            //int n = Convert.ToInt32(Console.ReadLine());
 
-            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-            ;
+            //int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int[] arr = new int[] { 4, 3, 1, 2 };
+
             int res = minimumSwaps(arr);
 
-            textWriter.WriteLine(res);
+            //textWriter.WriteLine(res);
 
-            textWriter.Flush();
-            textWriter.Close();
+            //textWriter.Flush();
+            //textWriter.Close();
         }
 
-        static int minimumSwaps(int[] arr)
+        static int minimumSwaps(int[] arr) // 4, 3, 1, 2  |  1, 2, 3 ,4
         {
+            // 4 3 1 2  0
+            // 2 3 1 4  1
+            // 3 2 1 4  2
+            // 1 2 3 4  3
 
-            return arr[0];
+            int swaps = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != i + 1)
+                {
+                    int temp = arr[i];
+                    int temp2 = arr[temp - 1];
+                    arr[temp - 1] = temp;
+                    arr[i] = temp2;
+                    swaps++;
+                    i = arr[i] == i + 1 ? i : i - 1;
+                }
+            }
+            return swaps;
         }
     }
 }
