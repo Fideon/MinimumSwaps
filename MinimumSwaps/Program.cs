da9d2f59222cc17f4dee70fcc5b283cf34e9f7de
Fidel Salazar <fidelsf91@gmail.com>
2020-11-04 21:32:22 -0600

    initial project commit

---
+++ MinimumSwaps/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace MinimumSwaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int res = minimumSwaps(arr);
+
+            textWriter.WriteLine(res);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        static int minimumSwaps(int[] arr)
+        {
+
+            return arr[0];
+        }
+    }
+}
